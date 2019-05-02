d27e040ab359c9e9cfd5a46bab3e899b54810c08
ccortes15 <47857246+ccortes15@users.noreply.github.com>
2019-05-02 07:45:08 -0500

    clase Persona

---
+++ Persona.js
@@ -0,0 +1,18 @@
+class Persona{
+    constructor(name){
+        this.name  = name
+        this.posicion = posicion
+    }
+
+    getName(){
+        return this.name
+    }
+
+    getPosicion(){
+        return this.posicion
+    }
+
+    setPosicion(posicion){
+        this.posicion = posicion
+    }
+}
